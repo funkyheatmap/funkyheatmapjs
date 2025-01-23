d527a4de4187abab759cfb54cc5cc6bffa588ac8
Nikolay Markov <nikolai.markov@icloud.com>
2025-01-22 21:46:27 -0600

    docs: position options and redo palettes

--- src/palettes.js
+++ src/palettes.js
@@ -27,63 +27,92 @@ import * as d3 from 'd3';
  */
 
 /**
- * Default palettes for numerical and categorical data. See source for the colors.
+ * Default palettes for numerical and categorical data.
  * @constant {Object}
- * @property {Object} numerical - palettes for numerical data. Default is `Blues`. See
- *   {@link module:palettes~numerical|numerical}
- * @property {Object} categorical - palettes for categorical data. Default is `Set1`. See
- *   {@link module:palettes~categorical|categorical}
+ * @property {Object} numerical - palettes for numerical data
+ * @property {string[]} numerical.Blues - <div class="inline-pal-preview" style="background: #011636"></div>
+ *   <div class="inline-pal-preview" style="background: #08306B"></div>
+ *   <div class="inline-pal-preview" style="background: #08519C"></div>
+ *   <div class="inline-pal-preview" style="background: #2171B5"></div>
+ *   <div class="inline-pal-preview" style="background: #4292C6"></div>
+ *   <div class="inline-pal-preview" style="background: #6BAED6"></div>
+ *   <div class="inline-pal-preview" style="background: #9ECAE1"></div>
+ *   <div class="inline-pal-preview" style="background: #C6DBEF"></div>
+ *   <div class="inline-pal-preview" style="background: #DEEBF7"></div>
+ *   <div class="inline-pal-preview" style="background: #F7FBFF"></div>&nbsp;. Default for
+ *   numerical data
+ * @property {string[]} numerical.Greens - <div class="inline-pal-preview" style="background: #00250f"></div>
+ *   <div class="inline-pal-preview" style="background: #00441B"></div>
+ *   <div class="inline-pal-preview" style="background: #006D2C"></div>
+ *   <div class="inline-pal-preview" style="background: #238B45"></div>
+ *   <div class="inline-pal-preview" style="background: #41AB5D"></div>
+ *   <div class="inline-pal-preview" style="background: #74C476"></div>
+ *   <div class="inline-pal-preview" style="background: #A1D99B"></div>
+ *   <div class="inline-pal-preview" style="background: #C7E9C0"></div>
+ *   <div class="inline-pal-preview" style="background: #E5F5E0"></div>
+ * @property {string[]} numerical.Greys - <div class="inline-pal-preview" style="background: #000000"></div>
+ *   <div class="inline-pal-preview" style="background: #252525"></div>
+ *   <div class="inline-pal-preview" style="background: #525252"></div>
+ *   <div class="inline-pal-preview" style="background: #737373"></div>
+ *   <div class="inline-pal-preview" style="background: #969696"></div>
+ *   <div class="inline-pal-preview" style="background: #BDBDBD"></div>
+ *   <div class="inline-pal-preview" style="background: #D9D9D9"></div>
+ *   <div class="inline-pal-preview" style="background: #F0F0F0"></div>
+ * @property {string[]} numerical.Reds - <div class="inline-pal-preview" style="background: #CB181D"></div>
+ *   <div class="inline-pal-preview" style="background: #EF3B2C"></div>
+ *   <div class="inline-pal-preview" style="background: #FB6A4A"></div>
+ *   <div class="inline-pal-preview" style="background: #FC9272"></div>
+ *   <div class="inline-pal-preview" style="background: #FCBBA1"></div>
+ *   <div class="inline-pal-preview" style="background: #FEE0D2"></div>
+ *   <div class="inline-pal-preview" style="background: #FFF5F0"></div>
+ * @property {string[]} numerical.YlOrBr - <div class="inline-pal-preview" style="background: #EC7014"></div>
+ *   <div class="inline-pal-preview" style="background: #FE9929"></div>
+ *   <div class="inline-pal-preview" style="background: #FEC44F"></div>
+ *   <div class="inline-pal-preview" style="background: #FEE391"></div>
+ *   <div class="inline-pal-preview" style="background: #FFF7BC"></div>
+ *   <div class="inline-pal-preview" style="background: #FFFFE5"></div>
+ * @property {Object} categorical - palettes for categorical data
+ * @property {string[]} categorical.Set1 - <div class="inline-pal-preview" style="background: #E41A1C"></div>
+ *   <div class="inline-pal-preview" style="background: #377EB8"></div>
+ *   <div class="inline-pal-preview" style="background: #4DAF4A"></div>
+ *   <div class="inline-pal-preview" style="background: #984EA3"></div>
+ *   <div class="inline-pal-preview" style="background: #FF7F00"></div>
+ *   <div class="inline-pal-preview" style="background: #FFFF33"></div>
+ *   <div class="inline-pal-preview" style="background: #A65628"></div>
+ *   <div class="inline-pal-preview" style="background: #F781BF"></div>
+ *   <div class="inline-pal-preview" style="background: #999999"></div>&nbsp;. Default for
+ *   categorical data
+ * @property {string[]} categorical.Set2 - <div class="inline-pal-preview" style="background: #66C2A5"></div>
+ *   <div class="inline-pal-preview" style="background: #FC8D62"></div>
+ *   <div class="inline-pal-preview" style="background: #8DA0CB"></div>
+ *   <div class="inline-pal-preview" style="background: #E78AC3"></div>
+ *   <div class="inline-pal-preview" style="background: #A6D854"></div>
+ *   <div class="inline-pal-preview" style="background: #FFD92F"></div>
+ *   <div class="inline-pal-preview" style="background: #E5C494"></div>
+ *   <div class="inline-pal-preview" style="background: #B3B3B3"></div>
+ * @property {string[]} categorical.Set3 - <div class="inline-pal-preview" style="background: #8DD3C7"></div>
+ *   <div class="inline-pal-preview" style="background: #FFFFB3"></div>
+ *   <div class="inline-pal-preview" style="background: #BEBADA"></div>
+ *   <div class="inline-pal-preview" style="background: #FB8072"></div>
+ *   <div class="inline-pal-preview" style="background: #80B1D3"></div>
+ *   <div class="inline-pal-preview" style="background: #FDB462"></div>
+ *   <div class="inline-pal-preview" style="background: #B3DE69"></div>
+ *   <div class="inline-pal-preview" style="background: #FCCDE5"></div>
+ *   <div class="inline-pal-preview" style="background: #D9D9D9"></div>
+ *   <div class="inline-pal-preview" style="background: #BC80BD"></div>
+ *   <div class="inline-pal-preview" style="background: #CCEBC5"></div>
+ *   <div class="inline-pal-preview" style="background: #FFED6F"></div>
+ * @property {string[]} categorical.Dark2 - <div class="inline-pal-preview" style="background: #1B9E77"></div>
+ *   <div class="inline-pal-preview" style="background: #D95F02"></div>
+ *   <div class="inline-pal-preview" style="background: #7570B3"></div>
+ *   <div class="inline-pal-preview" style="background: #E7298A"></div>
+ *   <div class="inline-pal-preview" style="background: #66A61E"></div>
+ *   <div class="inline-pal-preview" style="background: #E6AB02"></div>
+ *   <div class="inline-pal-preview" style="background: #A6761D"></div>
+ *   <div class="inline-pal-preview" style="background: #666666"></div>
  */
 const defaultPalettes = {
-    numerical:
-    /**
-     * @member numerical
-     * @abstract
-     * @description Palettes for numerical data. A property of
-     *   `{@link module:palettes~defaultPalettes|defaultPalettes`}.
-     * @property {string[]} Blues - <div class="inline-pal-preview" style="background: #011636"></div>
-     *   <div class="inline-pal-preview" style="background: #08306B"></div>
-     *   <div class="inline-pal-preview" style="background: #08519C"></div>
-     *   <div class="inline-pal-preview" style="background: #2171B5"></div>
-     *   <div class="inline-pal-preview" style="background: #4292C6"></div>
-     *   <div class="inline-pal-preview" style="background: #6BAED6"></div>
-     *   <div class="inline-pal-preview" style="background: #9ECAE1"></div>
-     *   <div class="inline-pal-preview" style="background: #C6DBEF"></div>
-     *   <div class="inline-pal-preview" style="background: #DEEBF7"></div>
-     *   <div class="inline-pal-preview" style="background: #F7FBFF"></div>&nbsp;. Default for
-     *   numerical data
-     * @property {string[]} Greens - <div class="inline-pal-preview" style="background: #00250f"></div>
-     *   <div class="inline-pal-preview" style="background: #00441B"></div>
-     *   <div class="inline-pal-preview" style="background: #006D2C"></div>
-     *   <div class="inline-pal-preview" style="background: #238B45"></div>
-     *   <div class="inline-pal-preview" style="background: #41AB5D"></div>
-     *   <div class="inline-pal-preview" style="background: #74C476"></div>
-     *   <div class="inline-pal-preview" style="background: #A1D99B"></div>
-     *   <div class="inline-pal-preview" style="background: #C7E9C0"></div>
-     *   <div class="inline-pal-preview" style="background: #E5F5E0"></div>
-     * @property {string[]} Greys - <div class="inline-pal-preview" style="background: #000000"></div>
-     *   <div class="inline-pal-preview" style="background: #252525"></div>
-     *   <div class="inline-pal-preview" style="background: #525252"></div>
-     *   <div class="inline-pal-preview" style="background: #737373"></div>
-     *   <div class="inline-pal-preview" style="background: #969696"></div>
-     *   <div class="inline-pal-preview" style="background: #BDBDBD"></div>
-     *   <div class="inline-pal-preview" style="background: #D9D9D9"></div>
-     *   <div class="inline-pal-preview" style="background: #F0F0F0"></div>
-     * @property {string[]} Reds - <div class="inline-pal-preview" style="background: #CB181D"></div>
-     *   <div class="inline-pal-preview" style="background: #EF3B2C"></div>
-     *   <div class="inline-pal-preview" style="background: #FB6A4A"></div>
-     *   <div class="inline-pal-preview" style="background: #FC9272"></div>
-     *   <div class="inline-pal-preview" style="background: #FCBBA1"></div>
-     *   <div class="inline-pal-preview" style="background: #FEE0D2"></div>
-     *   <div class="inline-pal-preview" style="background: #FFF5F0"></div>
-     * @property {string[]} YlOrBr - <div class="inline-pal-preview" style="background: #EC7014"></div>
-     *   <div class="inline-pal-preview" style="background: #FE9929"></div>
-     *   <div class="inline-pal-preview" style="background: #FEC44F"></div>
-     *   <div class="inline-pal-preview" style="background: #FEE391"></div>
-     *   <div class="inline-pal-preview" style="background: #FFF7BC"></div>
-     *   <div class="inline-pal-preview" style="background: #FFFFE5"></div>
-     */
-    {
+    numerical: {
         Blues: [
             "#011636", "#08306B", "#08519C", "#2171B5", "#4292C6", "#6BAED6", "#9ECAE1", "#C6DBEF",
             "#DEEBF7", "#F7FBFF"
@@ -102,52 +131,7 @@ const defaultPalettes = {
             "#EC7014", "#FE9929", "#FEC44F", "#FEE391", "#FFF7BC", "#FFFFE5"
         ]
     },
-    categorical:
-    /**
-     * @member categorical
-     * @abstract
-     * @description Palettes for categorical data. A property of
-     *   `{@link module:palettes~defaultPalettes|defaultPalettes`}.
-     * @property {string[]} Set1 - <div class="inline-pal-preview" style="background: #E41A1C"></div>
-     *   <div class="inline-pal-preview" style="background: #377EB8"></div>
-     *   <div class="inline-pal-preview" style="background: #4DAF4A"></div>
-     *   <div class="inline-pal-preview" style="background: #984EA3"></div>
-     *   <div class="inline-pal-preview" style="background: #FF7F00"></div>
-     *   <div class="inline-pal-preview" style="background: #FFFF33"></div>
-     *   <div class="inline-pal-preview" style="background: #A65628"></div>
-     *   <div class="inline-pal-preview" style="background: #F781BF"></div>
-     *   <div class="inline-pal-preview" style="background: #999999"></div>&nbsp;. Default for
-     *   categorical data
-     * @property {string[]} Set2 - <div class="inline-pal-preview" style="background: #66C2A5"></div>
-     *   <div class="inline-pal-preview" style="background: #FC8D62"></div>
-     *   <div class="inline-pal-preview" style="background: #8DA0CB"></div>
-     *   <div class="inline-pal-preview" style="background: #E78AC3"></div>
-     *   <div class="inline-pal-preview" style="background: #A6D854"></div>
-     *   <div class="inline-pal-preview" style="background: #FFD92F"></div>
-     *   <div class="inline-pal-preview" style="background: #E5C494"></div>
-     *   <div class="inline-pal-preview" style="background: #B3B3B3"></div>
-     * @property {string[]} Set3 - <div class="inline-pal-preview" style="background: #8DD3C7"></div>
-     *   <div class="inline-pal-preview" style="background: #FFFFB3"></div>
-     *   <div class="inline-pal-preview" style="background: #BEBADA"></div>
-     *   <div class="inline-pal-preview" style="background: #FB8072"></div>
-     *   <div class="inline-pal-preview" style="background: #80B1D3"></div>
-     *   <div class="inline-pal-preview" style="background: #FDB462"></div>
-     *   <div class="inline-pal-preview" style="background: #B3DE69"></div>
-     *   <div class="inline-pal-preview" style="background: #FCCDE5"></div>
-     *   <div class="inline-pal-preview" style="background: #D9D9D9"></div>
-     *   <div class="inline-pal-preview" style="background: #BC80BD"></div>
-     *   <div class="inline-pal-preview" style="background: #CCEBC5"></div>
-     *   <div class="inline-pal-preview" style="background: #FFED6F"></div>
-     * @property {string[]} Dark2 - <div class="inline-pal-preview" style="background: #1B9E77"></div>
-     *   <div class="inline-pal-preview" style="background: #D95F02"></div>
-     *   <div class="inline-pal-preview" style="background: #7570B3"></div>
-     *   <div class="inline-pal-preview" style="background: #E7298A"></div>
-     *   <div class="inline-pal-preview" style="background: #66A61E"></div>
-     *   <div class="inline-pal-preview" style="background: #E6AB02"></div>
-     *   <div class="inline-pal-preview" style="background: #A6761D"></div>
-     *   <div class="inline-pal-preview" style="background: #666666"></div>
-     */
-    {
+    categorical: {
         Set1: [
             "#E41A1C","#377EB8","#4DAF4A","#984EA3","#FF7F00","#FFFF33","#A65628","#F781BF",
             "#999999"
